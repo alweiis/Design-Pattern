362ee3a4dc3bbe6c572365c5e99e81f45557cd14
alweiis <jstm1207@gmail.com>
2023-10-06 14:57:42 +0900

    add Template Method Pattern

---
+++ src/tests/template_method/AbstractDisplay.java
@@ -0,0 +1,15 @@
+package tests.template_method;
+
+public interface AbstractDisplay {
+    void open();
+    void print();
+    void close();
+
+    default void display() {
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+}
